28f6793f69a282578f435ebb9712febc9846a54f
David Isailovic <74271327+david-isailovich@users.noreply.github.com>
2022-05-24 15:44:21 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let logo = document.querySelector('.lnXdpd');
+logo.src = chrome.runtime.getURL('images/cysecor_logo.png');
+logo.srcset = chrome.runtime.getURL('images/cysecor_logo.png');
+
+let logoV2 = document.querySelector('.logo');
+logo.src = chrome.runtime.getURL('images/cysecor_logo.png');
+logo.srcset = chrome.runtime.getURL('images/cysecor_logo.png');
+
